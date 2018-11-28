dd590141f7c795e7f29f61707ee89e32375eedc8
yokoryo <yokoyamar0@gmail.com>
2018-11-28 22:58:05 +0900

    01_02

---
+++ study/01_02/ex01/main.go
@@ -0,0 +1,21 @@
+// 1行のtxt形式のファイルを読み取り、内容すべて表示
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+
+	cmd := exec.Command("docker", *opt)
+	result, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+		fmt.Printf("出力結果: \n%s", result)
+}
+
