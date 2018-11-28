ceb65c73b787167b5f82405f3199a4f1b41cc358
yokoryo <yokoyamar0@gmail.com>
2018-11-28 23:01:13 +0900

    1128-3

--- study/01_02/ex01/main.go
+++ study/01_02/ex01/main.go
@@ -2,19 +2,8 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
 )
 
 func main() {
-
-	cmd := exec.Command("docker", *opt)
-	result, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-		fmt.Printf("出力結果: \n%s", result)
 }
 
